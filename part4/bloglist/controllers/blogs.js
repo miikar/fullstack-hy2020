6a0f6db1a56e8b778676a114f7886f46ecb65bb0
Miika Rouvinen <miika.j.rouvinen@gmail.com>
2021-11-11 17:00:46 +0200

    [part4] Completed 4.17

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,13 +1,23 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog
+    .find({}).populate('user', { username: 1, name: 1 })
+
   response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
+
+  // Get first user found
+  const users = await User.find({})
+
+  const user = users.length === 0
+    ? false
+    : users[0]
 
   if (body.title === undefined) {
     return response.status(400).json({ error: 'title missing' })
@@ -21,10 +31,14 @@ blogsRouter.post('/', async (request, response) => {
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: body.likes === undefined ? 0 : body.likes
+    likes: body.likes === undefined ? 0 : body.likes,
+    user: user._id
   })
 
   const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
   response.status(201).json(savedBlog)
 })
 
