7cf53ecf19b6129d97acee804c432e4b3464896f
Miika Rouvinen <miika.j.rouvinen@gmail.com>
2021-12-10 18:24:08 +0200

    [part7] Completed 7.1-7.3

---
+++ part7/routed-anecdotes/src/index.js
@@ -0,0 +1,10 @@
+import { BrowserRouter as Router } from 'react-router-dom'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>, 
+document.getElementById('root'))
